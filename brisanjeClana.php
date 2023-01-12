79ef1b09335f4a82abe54461eff68e29df53a5e3
Djordje Tornjanski <djordjepeka124@gmail.com>
2023-01-12 18:52:23 +0100

    Dodavanje funkcija

---
+++ brisanjeClana.php
@@ -0,0 +1,16 @@
+<?php
+
+$hostName = "localhost";
+$username = "root";
+$password = "";
+$dbname = "teretana";
+$connection = new mysqli($hostName, $username, $password, $dbname);
+
+$sqlQuery = "DELETE from clan where id=" . $_GET['clanId'];
+$connection->query($sqlQuery);
+
+echo "<script type='text/javascript'>
+    alert('Član je obrisan!');
+    location='prikazClanova.php';</script>";
+
+?>
