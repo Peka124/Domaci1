8092ba0f43e0bb576fba90745c521b32f46cd463
Djordje Tornjanski <djordjepeka124@gmail.com>
2023-01-12 18:52:48 +0100

    Dodavanje js funkcija

---
+++ javascript.js
@@ -0,0 +1,41 @@
+$(function () {
+  pretrazi();
+  sortiraj();
+});
+
+function pretrazi() {
+  $(document).on("click", "#pretraziBtn", function () {
+    var pretrazi = $("#txtPretraga").val();
+
+    $.ajax({
+      url: "pretraziClanove.php",
+      method: "post",
+      data: { Pretrazi: pretrazi },
+
+      success: function (result) {
+        {
+          $("#prikazBody").html(result);
+        }
+      },
+    });
+  });
+}
+
+function sortiraj() {
+  $(document).on("click", "#sortBtn", function () {
+    var kolonaSort = $("#selKolona").val();
+    var sort = $("#selSort").val();
+
+    $.ajax({
+      url: "sortirajClanove.php",
+      method: "post",
+      data: { KolonaSort: kolonaSort, Sort: sort },
+
+      success: function (result) {
+        {
+          $("#prikazBody").html(result);
+        }
+      },
+    });
+  });
+}
