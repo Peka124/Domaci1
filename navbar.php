c5ed8b3781887a018b1e59fc2366cfbfe8c739bb
Djordje Tornjanski <djordjepeka124@gmail.com>
2023-01-13 12:59:47 +0100

    Dodavanje navigacije

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light bg-success">
+    <div class="container-fluid">
+        <div class="nav-elementi">
+            <a class="navbar-brand text-white" id="pocetna-nav" href="index.php">Pocetna</a>
+            <a class="navbar-brand text-white" id="korisnici-nav" href="prikazClanova.php">Clanovi</a>
+        </div>
+    </div>
+</nav>
